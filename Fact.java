08faecba12c6e8776878193f929bafc844564bde
TigranMargaryan <margaryantigran4@gmail.com>
2018-08-13 17:21:59 +0400

    Ubdates

--- Fact.java
+++ Fact.java
@@ -1,5 +1,5 @@
 /*
- For example, 10! = 10 × 9 × ... × 3 × 2 × 1 = 3628800,
+ For example, 10! = 10 ï¿½ 9 ï¿½ ... ï¿½ 3 ï¿½ 2 ï¿½ 1 = 3628800,
 and the sum of the digits in the number 10! is 3 + 6 + 2 + 8 + 8 + 0 + 0 = 27.
 
 Find the sum of the digits in the number 100!
@@ -15,7 +15,7 @@ public class Fact {
 		BigInteger s = new BigInteger("1");
 		int y = 0;
 		
-		for(int i = 1; i<100; i++)
+		for(int i = 1; i<101; i++)
 			s = s.multiply(BigInteger.valueOf(i));
 		
 		String x = s.toString();
